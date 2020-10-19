64c47603307c799809a65b8c1308157b085ed81b
Reza Fadilah <31206753+rezafd23@users.noreply.github.com>
2020-10-19 22:29:47 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-var dataAll=[]
+var dataAll=[{
+    name: "admin",
+    email: "admin@admin.com",
+    password: "admin",
+    tglLahir: "21-10-2020",
+    jenisKelamin: "L",
+}]
 var idnumber
 var formLogin=document.getElementById("formLogin");
 var formRegister=document.getElementById("formRegister");
@@ -57,60 +63,100 @@ function showComponent(id){
     id.hidden=false
     // id.style.visibility='visible'
 }
-function saveData() {
-    // let nama=document.formRegister.nameForm.value;
-    let nama=document.formRegister.nameForm.value;
-    let email=document.formRegister.emailForm.value;
-    let password=document.formRegister.passwordForm.value;
-    let tglLahir=document.formRegister.birthdayForm.value;
-    let jenisKelamin=document.formRegister.gender.value;
 
-    let dataArray=[nama,email,password,tglLahir,jenisKelamin]
-    let i = dataAll.length
-    dataAll[i]=dataArray
-    // console.log(dataAll)
+var saveDataUser=()=>{
+    let form=document.formRegister
+    let nama=form.nameForm.value;
+    let email=form.emailForm.value;
+    let password=form.passwordForm.value;
+    let tglLahir=form.birthdayForm.value;
+    let jenisKelamin=form.gender.value;
+    let idxUser=dataAll.length
+    let saveData=false
+    let id=form.id.value
 
-    // showData(dataAll)
+    if (nama&&email&&password&&tglLahir&&jenisKelamin){
+        let used=false
+        // Map
+        // used=dataAll.map(obj=>{
+        //     if (obj.email == email) return true
+        //
+        // })
+        // find
+        used=dataAll.find(obj=>{
+            if (obj.email==email) return true
+        })
+
+        if (id) used=false
+        // for (i=0;i<dataAll.length;i++){
+        //     let user = dataAll[i]
+        //     if (email==user.email){
+        //         used=true
+        //         break
+        //     }
+        // }
+        if (used){
+            alert("email telah digunakan")
+            return
+        }
+        let registrasiData={
+            name:nama,
+            email:email,
+            password:password,
+            tglLahir:tglLahir,
+            jenisKelamin:jenisKelamin
+        }
+        if (id){
+            dataAll.splice(parseInt(id),1,registrasiData)
+        } else {
+            dataAll.splice(idxUser,1,registrasiData)
+        }
+        saveData=true
+        form.reset()
+    } else {
+        alert("Mohon Lengkapi Data")
+    }
+    console.log(dataAll)
+
+    return saveData;
+
 }
 function register(){
-    showComponent(formLogin)
-    hideComponent(formRegister)
-    saveData()
+    if (saveDataUser()){
+        showComponent(formLogin)
+        hideComponent(formRegister)
+    }
 }
 function goRegister(){
     showComponent(formRegister)
     hideComponent(formLogin)
 }
-function login(){
-    let email=document.formLogin.emailForm.value;
-    let password=document.formLogin.passwordForm.value;
-    for (i=0;i<dataAll.length;i++){
-        // console.log("isi data "+data[i])
-        if (email===dataAll[i][1]){
-            if (password==dataAll[i][2]){
-                showComponent(formBeranda)
-                showComponent(navBar)
-                hideComponent(formLogin)
-                showData(dataAll)
-            } else {
-                console.log("ini1")
-                alert("Mohon Periksa Kembali Email dan Password")
-            }
-        }else {
-            console.log("ini2")
-            alert("Mohon Periksa Kembali Email dan Password")
+var doLogin=()=>{
+    let form = document.formLogin
+    email=form.emailForm.value
+    password=form.passwordForm.value
 
+    if (email&&password){
+        let statusLogin=false
+        statusLogin=dataAll.map((data)=>{
+            if (data.email==email&&data.password==password) return true
+        })
+        // for (i=0;i<dataAll.length;i++){
+        //     let data=dataAll[i]
+        //     if (email==data.email&&password==data.password){
+        //         statusLogin=true
+        //         break
+        //     }
+        // }
+        if (statusLogin){
+            showComponent(formBeranda)
+            showComponent(navBar)
+            hideComponent(formLogin)
+            showUserData()
+            form.reset()
+        } else {
+            alert("Mohon Cek Email dan Password")
         }
-        // tr += `
-        //     <tr>
-        //         <td>${i + 1}</td>
-        //         <td>${data[i][0]}</td>
-        //         <td>${data[i][1]}</td>
-        //         <td>${data[i][2]}</td>
-        //         <td>${data[i][3]}</td>
-        //         <td>${data[i][4]}</td>
-        //     </tr>
-        // `
     }
 }
 function saveEdit(){
@@ -127,8 +173,32 @@ function saveEdit(){
     // console.log(dataAll)
     dataAll.splice(id,1,dataArray);
     showData(dataAll)
+    document.formEdit.reset()
 }
+var showUserData=()=>{
+    let tableData=document.getElementsByClassName("tableData")[0]
+    let tr=tableData.children[0].children[0].outerHTML
 
+    for (i=0;i<dataAll.length;i++){
+        let data=dataAll[i]
+        tr += `
+            <tr align="center">
+                <td>${i + 1}</td>
+                <td>${data.name}</td>
+                <td>${data.email}</td>
+                <td>${data.password}</td>
+                <td>${data.tglLahir}</td>
+                <td>${data.jenisKelamin}</td>
+                <td>
+                <button onclick="editData(${i})">Update</button>
+                <button onclick="deleteData(${i})">Delete</button>
+                </td>
+            </tr>
+        `
+    }
+    tableData.innerHTML=tr
+}
+showUserData()
 function showData(data) {
     let tableData=document.getElementsByClassName("tableData")[0]
     let tr=tableData.children[0].children[0].outerHTML
@@ -152,15 +222,18 @@ function showData(data) {
     tableData.innerHTML=tr
 }
 
-function editData(idnumber){
-    document.formEdit.nameForm.value=dataAll[idnumber][0]
-    document.formEdit.emailForm.value=dataAll[idnumber][1]
-    document.formEdit.passwordForm.value=dataAll[idnumber][2]
-    document.formEdit.birthdayForm.value=dataAll[idnumber][3]
-    document.formEdit.gender.value=dataAll[idnumber][4]
-    document.formEdit.id.value=idnumber
+
+var editData=idnumber=>{
+    showRegister()
+    let form = document.formRegister
+    form.nameForm.value=dataAll[idnumber].name
+    form.emailForm.value=dataAll[idnumber].email
+    form.passwordForm.value=dataAll[idnumber].password
+    form.birthdayForm.value=dataAll[idnumber].tglLahir
+    form.gender.value=dataAll[idnumber].jenisKelamin
+    form.id.value=idnumber
 }
-function deleteData(id){
+var deleteData=id=>{
     // dataAll=dataAll.slice(0);
     dataAll.splice(id-1,1);
     showData(dataAll)
---
+++ js/script2.js
@@ -0,0 +1,258 @@
+var userLogin = {},
+    userList = [{
+        name: "admin",
+        email: "admin@admin.com",
+        password: "admin",
+        gender: "L",
+        age: 21
+    }, {
+        name: "admin1",
+        email: "admin1@admin.com",
+        password: "admin1",
+        gender: "P",
+        age: 22
+    }, {
+        name: "admin2",
+        email: "admin2@admin.com",
+        password: "admin2",
+        gender: "L",
+        age: 23
+    }]
+
+var getAll = function (attr) {
+    return document.querySelectorAll(attr)
+}
+
+var get = function (attr) {
+    return document.querySelector(attr)
+}
+
+var onMenuClicked = function (e) {
+    var target = e.target.getAttribute("content")
+    hideLastPage()
+    showPage(target)
+}
+
+var hideLastPage = function () {
+    var menu = get(".navbar-list.active")
+    var page = get(".body-content.active")
+    menu.classList.remove("active")
+    page.classList.remove("active")
+}
+
+var showPage = function (target) {
+    var menu = get(".navbar-list[content='" + target + "']")
+    var page = get(".body-content[menu='" + target + "']")
+    menu.classList.add("active")
+    page.classList.add("active")
+
+    // if (cb) cb()
+    if (target == "home") renderHome()
+}
+
+var register = function () {
+    var form = document.register
+    var name = form.nama.value
+    var email = form.email.value
+    var password = form.password.value
+    var gender = form.jk.value
+    var id = form.id.value // admin@admin.com
+    var idxUser = userList.length
+
+    if (name && email && password && gender) {
+        var used = false
+
+        for (let index = 0; index < userList.length; index++) {
+            var user = userList[index];
+
+            if (email == user.email) {
+
+                if (id) {
+                    idxUser = index
+                    break
+                }
+
+                used = true
+                break
+            }
+        }
+
+        if (used) {
+            alert("Email telah digunakan!!")
+            return
+        }
+
+        var dataRegister = {
+            name: name,
+            email: email,
+            password: password,
+            gender: gender,
+        }
+        // userList.push(dataRegister)
+        userList.splice(idxUser, 1, dataRegister)
+        alert("Data telah tersimpan!!")
+        form.reset()
+        form.email.readOnly = false
+    } else alert("Harap lengkapi data Anda!!")
+}
+
+var doLogin = function () {
+    var form = document.login
+    var email = form.email.value
+    var password = form.password.value
+
+    if (email && password) {
+        var statusLogin = false
+        for (let index = 0; index < userList.length; index++) {
+            var user = userList[index];
+
+            if (email == user.email && password == user.password) {
+                statusLogin = true
+                userLogin = user
+                break
+            }
+        }
+
+        if (statusLogin) {
+            alert("Sukses login!!\nHai " + userLogin.name)
+            form.reset()
+            afterLogin()
+        } else {
+            alert("Email dan Password salah!!")
+        }
+    } else alert("Email dan Password tidak boleh kosong!!")
+}
+
+var afterLogin = function () {
+    get(".navbar-list[content='keluar']").classList.remove("hidden")
+    get(".navbar-list[content='masuk']").classList.add("hidden")
+    hideLastPage()
+    showPage("home")
+}
+
+var doLogout = function () {
+    userLogin = {}
+    get(".navbar-list[content='keluar']").classList.add("hidden")
+    get(".navbar-list[content='masuk']").classList.remove("hidden")
+}
+
+var renderHome = function () {
+    var tableUsers = get("table[data='user']")
+    var newTR = get("table[data='user'] tr").innerHTML
+
+    for (let index = 0; index < userList.length; index++) {
+        var user = userList[index];
+
+        newTR += `
+            <tr>
+                <td align="right">${index + 1}</td>
+                <td>${user.name}</td>
+                <td>
+                    ${(user.gender == "L") ? "Laki-laki" : (user.gender == "P" ? "Perempuan" : "")}
+                </td>
+                <td align="center">
+                    <button onclick="editUser('${user.email}')">Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        `
+    }
+    tableUsers.innerHTML = newTR
+}
+
+var editUser = function (email) {
+    console.log(email)
+    hideLastPage()
+    showPage("daftar")
+
+    var selectedUser = ""
+    for (let index = 0; index < userList.length; index++) {
+        const user = userList[index];
+
+        if (user.email == email) {
+            selectedUser = user
+            break
+        }
+    }
+
+    if (selectedUser) {
+        var form = document.register
+        form.nama.value = selectedUser.name
+        form.email.value = selectedUser.email
+        form.email.readOnly = true
+        form.password.value = selectedUser.password
+        form.jk.value = selectedUser.gender
+        form.id.value = selectedUser.email
+    } else alert("Data tidak dapat ditemukan!!")
+}
+
+var init = function () {
+    // add event to list menu
+    var navbarList = getAll(".navbar-list:not([content='keluar'])")
+    for (i = 0; i < navbarList.length; i++) {
+        var element = navbarList[i];
+        element.addEventListener("click", onMenuClicked)
+    }
+
+    // add event to logout menu
+    var buttonReg = get(".navbar-list[content='keluar']")
+    buttonReg.addEventListener("click", doLogout)
+
+    // add event to register button
+    var buttonReg = get("#buttonRegister")
+    buttonReg.addEventListener("click", register)
+
+    // add event to login button
+    var buttonReg = get("#buttonLogin")
+    buttonReg.addEventListener("click", doLogin)
+
+
+    showPage("home")
+}
+
+init()
+
+
+function a(param1, param2, cb) {
+    let s = param1 + param2
+    // logic
+    return cb(s, param2)
+    // return param1 + param2
+}
+
+
+var c = (param1, param2) => {
+    return param1 + param2
+}
+
+
+const result = userList.filter((val, idx) => val.gender == "P")
+console.warn(result)
+
+const resultMap = userList.map((val, idx) => {
+    if (val.name == "admin") {
+        return "admin"
+    }
+
+    return {
+        name: val.name,
+        gender: val.gender == "L" ? "Laki-laki" : "Perempuan",
+        email: val.email + ".com"
+    }
+})
+console.log(resultMap);
+
+const resultRed = userList.reduce((ret, data) => ret + data.age, 0)
+console.log(resultRed);
+
+let total = 0
+userList.forEach(val => {
+    console.warn(val)
+    total += val.age
+})
+console.log(total);
+
+/*
+    Tugas:
+        - Ganti metode2 yang kalian gunakan (for) menggunakan reduce/filter/map/find
+*/
